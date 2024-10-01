c5e6a1e571263e52a35c0108112477b9f8660b46
chimcutdimua <kietle10085@gmail.com>
2024-10-01 13:56:54 +0700

    complete signup function

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,16 +1,64 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
+    const navigate = useNavigate()
+    const [formData, setFormData] = useState({})
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value,
+        })
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setLoading(true)
+        const { username, email, password } = formData;
+        if (!username || !email || !password) {
+            setError("All fields are required");
+            setLoading(false);
+            return;
+        }
+        try {
+            const res = await fetch('/api/auth/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            })
+            const data = await res.json()
+            if (data.success === false) {
+                setError(data.message)
+                setLoading(false)
+                return
+            }
+            if (data.message === "Username or email already exists") {
+                setError(data.message)
+                setLoading(false)
+                return;
+            }
+            setLoading(false)
+            navigate('/log-in', { replace: true })
+            setFormData({})
+            setError(null)
+        } catch (err) {
+            setLoading(false)
+            setError(err.message)
+        }
+    }
     return (
         <div className='max-w-lg mx-auto p-3'>
             <h1 className='text-3xl font-bold text-center my-7'>Sign Up</h1>
-            <form className='flex flex-col gap-4'>
-                <input className='border p-3 rounded-lg' type="text" placeholder='username' id='username' />
-                <input className='border p-3 rounded-lg' type="email" placeholder='email' id='email' />
-                <input className='border p-3 rounded-lg' type="password" placeholder='password' id='password' />
-                <button className='bg-stone-700 text-white rounded-lg uppercase p-3 hover:opacity-95 disabled:opacity-80'>
-                    Sign Up
+            <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+                <input className='border p-3 rounded-lg' type="text" placeholder='username' id='username' onChange={handleChange} />
+                <input className='border p-3 rounded-lg' type="email" placeholder='email' id='email' onChange={handleChange} />
+                <input className='border p-3 rounded-lg' type="password" placeholder='password' id='password' onChange={handleChange} />
+                <button disabled={loading} className='bg-stone-700 text-white rounded-lg uppercase p-3 hover:opacity-95 disabled:opacity-80'>
+                    {loading ? 'Loading...' : 'Sign Up'}
                 </button>
             </form>
             <div className='flex gap-2 mt-5'>
@@ -19,6 +67,7 @@ const SignUp = () => {
                     Sign in
                 </Link>
             </div>
+            {error && <p className='text-red-500 mt-5'>{error}</p>}
         </div>
     )
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3001", // Replace with your backend API endpoint
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
