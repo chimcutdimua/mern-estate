156fd55daf90627282474c64893d7512c093f524
chimcutdimua <kietle10085@gmail.com>
2024-10-01 15:44:49 +0700

    add redux toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom'
+import { store } from './redux/store.js'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
 )
--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice'
 
 const LogIn = () => {
     const navigate = useNavigate()
+    const dispatch = useDispatch()
     const [formData, setFormData] = useState({})
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(false)
-
+    const { loading, error } = useSelector((state) => state.user)
     const handleChange = (e) => {
         setFormData({
             ...formData,
@@ -15,14 +16,13 @@ const LogIn = () => {
     }
     const handleSubmit = async (e) => {
         e.preventDefault()
-        setLoading(true)
-        const { email, password } = formData;
+        const { email, password } = formData
         if (!email || !password) {
-            setError("All fields are required");
-            setLoading(false);
-            return;
+            dispatch(signInFailure('Email and password are required'))
+            return
         }
         try {
+            dispatch(signInStart())
             const res = await fetch('/api/auth/signin', {
                 method: 'POST',
                 headers: {
@@ -32,22 +32,17 @@ const LogIn = () => {
             })
             const data = await res.json()
             if (data.success === false) {
-                setError(data.message)
-                setLoading(false)
+                dispatch(signInFailure(data.message))
                 return
             }
-            if (data.message === "Invalid credentials") {
-                setError("Invalid credentials")
-                setLoading(false)
+            if (data.message === 'Invalid credentials') {
+                dispatch(signInFailure(data.message))
                 return
             }
-            setLoading(false)
-            setFormData({})
-            setError(null)
+            dispatch(signInSuccess(data))
             navigate('/')
         } catch (err) {
-            setLoading(false)
-            setError(err.message)
+            dispatch(signInFailure(err.message))
         }
     }
     return (
