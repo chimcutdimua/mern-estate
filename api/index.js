ded45cc22fa0796ec47602fe5487387e95e8c859
chimcutdimua <kietle10085@gmail.com>
2024-10-08 13:48:29 +0700

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,7 +5,9 @@ const userRouter = require("./routes/user.route");
 const authRouter = require("./routes/auth.route");
 const listingRouter = require("./routes/listing.route");
 const { default: mongoose } = require("mongoose");
+const path = require("path");
 const app = express();
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 
@@ -28,6 +30,12 @@ app.listen(3001, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
